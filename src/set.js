385a3802f778c0e6b940764705fc59908c0b31cf
Justin Meyer <justinbmeyer@gmail.com>
2015-04-21 20:27:22 -0500

    publishing to the window

--- src/set.js
+++ src/set.js
@@ -1,4 +1,8 @@
 var set = require("./set-core");
 var comparators = require("./comparators");
 set.comparators = comparators;
+if(typeof window !== "undefined" && !require.resolve) {
+	window.set = set;
+}
+
 module.exports = set;
