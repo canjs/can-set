374828f622dddde7e88992c0b1a20eca443bfcbc
Justin Meyer <justinbmeyer@gmail.com>
2016-04-06 18:06:05 -0500

    0.5.3

---
+++ dist/cjs/src/compare.js
@@ -0,0 +1,450 @@
+/*src/compare*/
+var h = require('./helpers.js');
+var compareHelpers;
+var loop = function (a, b, aParent, bParent, prop, compares, options) {
+    var checks = options.checks;
+    for (var i = 0; i < checks.length; i++) {
+        var res = checks[i](a, b, aParent, bParent, prop, compares || {}, options);
+        if (res !== undefined) {
+            return res;
+        }
+    }
+    return options['default'];
+};
+var addIntersectedPropertyToResult = function (a, b, aParent, bParent, prop, compares, options) {
+    var subsetCheck;
+    if (!(prop in aParent)) {
+        subsetCheck = 'subsetB';
+    } else if (prop in bParent) {
+        return false;
+    }
+    if (!(prop in bParent)) {
+        subsetCheck = 'subsetA';
+    }
+    if (subsetCheck === 'subsetB') {
+        options.result[prop] = b;
+    } else {
+        options.result[prop] = a;
+    }
+    return undefined;
+};
+var addToResult = function (fn, name) {
+    return function (a, b, aParent, bParent, prop, compares, options) {
+        var res = fn.apply(this, arguments);
+        if (res === true) {
+            if (prop !== undefined && !(prop in options.result)) {
+                options.result[prop] = a;
+            }
+            return true;
+        } else {
+            return res;
+        }
+    };
+};
+module.exports = compareHelpers = {
+    equal: function (a, b, aParent, bParent, prop, compares, options) {
+        options.checks = [
+            compareHelpers.equalComparesType,
+            compareHelpers.equalBasicTypes,
+            compareHelpers.equalArrayLike,
+            compareHelpers.equalObject
+        ];
+        options['default'] = false;
+        return loop(a, b, aParent, bParent, prop, compares, options);
+    },
+    equalComparesType: function (a, b, aParent, bParent, prop, compares, options) {
+        if (typeof compares === 'function') {
+            var compareResult = compares(a, b, aParent, bParent, prop, options);
+            if (typeof compareResult === 'boolean') {
+                return compareResult;
+            } else if (compareResult && typeof compareResult === 'object') {
+                if ('intersection' in compareResult && !('difference' in compareResult)) {
+                    var reverseResult = compares(b, a, bParent, aParent, prop, options);
+                    return 'intersection' in reverseResult && !('difference' in reverseResult);
+                }
+                return false;
+            }
+            return compareResult;
+        }
+    },
+    equalBasicTypes: function (a, b, aParent, bParent, prop, compares, options) {
+        if (a === null || b === null) {
+            return a === b;
+        }
+        if (a instanceof Date || b instanceof Date) {
+            return a === b;
+        }
+        if (options.deep === -1) {
+            return typeof a === 'object' || a === b;
+        }
+        if (typeof a !== typeof b || h.isArrayLike(a) !== h.isArrayLike(b)) {
+            return false;
+        }
+        if (a === b) {
+            return true;
+        }
+    },
+    equalArrayLike: function (a, b, aParent, bParent, prop, compares, options) {
+        if (h.isArrayLike(a) && h.isArrayLike(b)) {
+            if (a.length !== b.length) {
+                return false;
+            }
+            for (var i = 0; i < a.length; i++) {
+                var compare = compares[i] === undefined ? compares['*'] : compares[i];
+                if (!loop(a[i], b[i], a, b, i, compare, options)) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    },
+    equalObject: function (a, b, aParent, bParent, parentProp, compares, options) {
+        var aType = typeof a;
+        if (aType === 'object' || aType === 'function') {
+            var bCopy = h.extend({}, b);
+            if (options.deep === false) {
+                options.deep = -1;
+            }
+            for (var prop in a) {
+                var compare = compares[prop] === undefined ? compares['*'] : compares[prop];
+                if (!loop(a[prop], b[prop], a, b, prop, compare, options)) {
+                    return false;
+                }
+                delete bCopy[prop];
+            }
+            for (prop in bCopy) {
+                if (compares[prop] === undefined || !loop(undefined, b[prop], a, b, prop, compares[prop], options)) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    },
+    subset: function (a, b, aParent, bParent, prop, compares, options) {
+        options.checks = [
+            compareHelpers.subsetComparesType,
+            compareHelpers.equalBasicTypes,
+            compareHelpers.equalArrayLike,
+            compareHelpers.subsetObject
+        ];
+        options.getSubsets = [];
+        options['default'] = false;
+        return loop(a, b, aParent, bParent, prop, compares, options);
+    },
+    subsetObject: function (a, b, aParent, bParent, parentProp, compares, options) {
+        var aType = typeof a;
+        if (aType === 'object' || aType === 'function') {
+            return h.eachInUnique(a, function (a, b, aParent, bParent, prop) {
+                var compare = compares[prop] === undefined ? compares['*'] : compares[prop];
+                if (!loop(a, b, aParent, bParent, prop, compare, options) && prop in bParent) {
+                    return false;
+                }
+            }, b, function (a, b, aParent, bParent, prop) {
+                var compare = compares[prop] === undefined ? compares['*'] : compares[prop];
+                if (!loop(a, b, aParent, bParent, prop, compare, options)) {
+                    return false;
+                }
+            }, true);
+        }
+    },
+    subsetComparesType: function (a, b, aParent, bParent, prop, compares, options) {
+        if (typeof compares === 'function') {
+            var compareResult = compares(a, b, aParent, bParent, prop, options);
+            if (typeof compareResult === 'boolean') {
+                return compareResult;
+            } else if (compareResult && typeof compareResult === 'object') {
+                if (compareResult.getSubset) {
+                    if (h.indexOf.call(options.getSubsets, compareResult.getSubset) === -1) {
+                        options.getSubsets.push(compareResult.getSubset);
+                    }
+                }
+                if (compareResult.intersection === h.ignoreType || compareResult.difference === h.ignoreType) {
+                    return true;
+                }
+                if ('intersection' in compareResult && !('difference' in compareResult)) {
+                    var reverseResult = compares(b, a, bParent, aParent, prop, options);
+                    return 'intersection' in reverseResult;
+                }
+                return false;
+            }
+            return compareResult;
+        }
+    },
+    properSupersetObject: function (a, b, aParent, bParent, parentProp, compares, options) {
+        var bType = typeof b;
+        var hasAdditionalProp = false;
+        if (bType === 'object' || bType === 'function') {
+            var aCopy = h.extend({}, a);
+            if (options.deep === false) {
+                options.deep = -1;
+            }
+            for (var prop in b) {
+                var compare = compares[prop] === undefined ? compares['*'] : compares[prop];
+                var compareResult = loop(a[prop], b[prop], a, b, prop, compare, options);
+                if (compareResult === h.ignoreType) {
+                } else if (!(prop in a) || options.performedDifference) {
+                    hasAdditionalProp = true;
+                } else if (!compareResult) {
+                    return false;
+                }
+                delete aCopy[prop];
+            }
+            for (prop in aCopy) {
+                if (compares[prop] === undefined || !loop(a[prop], undefined, a, b, prop, compares[prop], options)) {
+                    return false;
+                }
+            }
+            return hasAdditionalProp;
+        }
+    },
+    properSubsetComparesType: function (a, b, aParent, bParent, prop, compares, options) {
+        if (typeof compares === 'function') {
+            var compareResult = compares(a, b, aParent, bParent, prop, options);
+            if (typeof compareResult === 'boolean') {
+                return compareResult;
+            } else if (compareResult && typeof compareResult === 'object') {
+                if ('intersection' in compareResult && !('difference' in compareResult)) {
+                    var reverseResult = compares(b, a, bParent, aParent, prop, options);
+                    return 'intersection' in reverseResult && 'difference' in reverseResult;
+                }
+                return false;
+            }
+            return compareResult;
+        }
+    },
+    difference: function (a, b, aParent, bParent, prop, compares, options) {
+        options.result = {};
+        options.performedDifference = 0;
+        options.checks = [
+            compareHelpers.differenceComparesType,
+            addToResult(compareHelpers.equalBasicTypes, 'equalBasicTypes'),
+            addToResult(compareHelpers.equalArrayLike, 'equalArrayLike'),
+            addToResult(compareHelpers.properSupersetObject, 'properSubsetObject')
+        ];
+        options['default'] = true;
+        var res = loop(a, b, aParent, bParent, prop, compares, options);
+        if (res === true && options.performedDifference) {
+            return options.result;
+        }
+        return res;
+    },
+    differenceComparesType: function (a, b, aParent, bParent, prop, compares, options) {
+        if (typeof compares === 'function') {
+            var compareResult = compares(a, b, aParent, bParent, prop, options);
+            if (typeof compareResult === 'boolean') {
+                if (compareResult === true) {
+                    options.result[prop] = a;
+                    return true;
+                } else {
+                    return compareResult;
+                }
+            } else if (compareResult && typeof compareResult === 'object') {
+                if ('difference' in compareResult) {
+                    if (compareResult.difference === h.ignoreType) {
+                        return h.ignoreType;
+                    } else if (compareResult.difference != null) {
+                        options.result[prop] = compareResult.difference;
+                        options.performedDifference++;
+                        return true;
+                    } else {
+                        return true;
+                    }
+                } else {
+                    if (compareHelpers.equalComparesType.apply(this, arguments)) {
+                        options.performedDifference++;
+                        options.result[prop] = compareResult.union;
+                    } else {
+                        return false;
+                    }
+                }
+            }
+        }
+    },
+    union: function (a, b, aParent, bParent, prop, compares, options) {
+        options.result = {};
+        options.performedUnion = 0;
+        options.checks = [
+            compareHelpers.unionComparesType,
+            addToResult(compareHelpers.equalBasicTypes, 'equalBasicTypes'),
+            addToResult(compareHelpers.unionArrayLike, 'unionArrayLike'),
+            addToResult(compareHelpers.unionObject, 'unionObject')
+        ];
+        options.getUnions = [];
+        options['default'] = false;
+        var res = loop(a, b, aParent, bParent, prop, compares, options);
+        if (res === true) {
+            return options.result;
+        }
+        return false;
+    },
+    unionComparesType: function (a, b, aParent, bParent, prop, compares, options) {
+        if (typeof compares === 'function') {
+            var compareResult = compares(a, b, aParent, bParent, prop, options);
+            if (typeof compareResult === 'boolean') {
+                if (compareResult === true) {
+                    options.result[prop] = a;
+                    return true;
+                } else {
+                    return compareResult;
+                }
+            } else if (compareResult && typeof compareResult === 'object') {
+                if (compareResult.getUnion) {
+                    if (h.indexOf.call(options.getUnions, compareResult.getUnion) === -1) {
+                        options.getUnions.push(compareResult.getUnion);
+                    }
+                }
+                if ('union' in compareResult) {
+                    if (compareResult.union === h.ignoreType) {
+                        return compareResult.union;
+                    }
+                    if (compareResult.union !== undefined) {
+                        options.result[prop] = compareResult.union;
+                    }
+                    options.performedUnion++;
+                    return true;
+                }
+            }
+        }
+    },
+    unionObject: function (a, b, aParent, bParent, prop, compares, options) {
+        var subsetCompare = function (a, b, aParent, bParent, prop) {
+            var compare = compares[prop] === undefined ? compares['*'] : compares[prop];
+            if (!loop(a, b, aParent, bParent, prop, compare, options)) {
+                var subsetCheck;
+                if (!(prop in aParent)) {
+                    subsetCheck = 'subsetB';
+                }
+                if (!(prop in bParent)) {
+                    subsetCheck = 'subsetA';
+                }
+                if (subsetCheck) {
+                    if (!options.subset) {
+                        options.subset = subsetCheck;
+                    }
+                    return options.subset === subsetCheck ? undefined : false;
+                }
+                return false;
+            }
+        };
+        var aType = typeof a;
+        if (aType === 'object' || aType === 'function') {
+            return h.eachInUnique(a, subsetCompare, b, subsetCompare, true);
+        }
+    },
+    unionArrayLike: function (a, b, aParent, bParent, prop, compares, options) {
+        if (h.isArrayLike(a) && h.isArrayLike(b)) {
+            var combined = h.makeArray(a).concat(h.makeArray(b));
+            h.doubleLoop(combined, function (item, j, cur, i) {
+                var res = !compareHelpers.equal(cur, item, aParent, bParent, undefined, compares['*'], { 'default': false });
+                return res;
+            });
+            options.result[prop] = combined;
+            return true;
+        }
+    },
+    count: function (a, b, aParent, bParent, prop, compares, options) {
+        options.checks = [
+            compareHelpers.countComparesType,
+            compareHelpers.equalBasicTypes,
+            compareHelpers.equalArrayLike,
+            compareHelpers.loopObject
+        ];
+        options['default'] = false;
+        loop(a, b, aParent, bParent, prop, compares, options);
+        if (typeof options.count === 'number') {
+            return options.count;
+        }
+        return Infinity;
+    },
+    countComparesType: function (a, b, aParent, bParent, prop, compares, options) {
+        if (typeof compares === 'function') {
+            var compareResult = compares(a, b, aParent, bParent, prop, options);
+            if (typeof compareResult === 'boolean') {
+                return true;
+            } else if (compareResult && typeof compareResult === 'object') {
+                if (typeof compareResult.count === 'number') {
+                    if (!('count' in options) || compareResult.count === options.count) {
+                        options.count = compareResult.count;
+                    } else {
+                        options.count = Infinity;
+                    }
+                }
+                return true;
+            }
+        }
+    },
+    loopObject: function (a, b, aParent, bParent, prop, compares, options) {
+        var aType = typeof a;
+        if (aType === 'object' || aType === 'function') {
+            h.each(a, function (aValue, prop) {
+                var compare = compares[prop] === undefined ? compares['*'] : compares[prop];
+                loop(aValue, b[prop], a, b, prop, compare, options);
+            });
+            return true;
+        }
+    },
+    intersection: function (a, b, aParent, bParent, prop, compares, options) {
+        options.result = {};
+        options.performedIntersection = 0;
+        options.checks = [
+            compareHelpers.intersectionComparesType,
+            addToResult(compareHelpers.equalBasicTypes, 'equalBasicTypes'),
+            addToResult(compareHelpers.intersectionArrayLike, 'intersectionArrayLike'),
+            compareHelpers.intersectionObject
+        ];
+        options['default'] = false;
+        var res = loop(a, b, aParent, bParent, prop, compares, options);
+        if (res === true) {
+            return options.result;
+        }
+        return false;
+    },
+    intersectionComparesType: function (a, b, aParent, bParent, prop, compares, options) {
+        if (typeof compares === 'function') {
+            var compareResult = compares(a, b, aParent, bParent, prop, options);
+            if (typeof compareResult === 'boolean') {
+                if (compareResult === true) {
+                    options.result[prop] = a;
+                    return true;
+                } else {
+                    return compareResult;
+                }
+            } else if (compareResult && typeof compareResult === 'object') {
+                if ('intersection' in compareResult) {
+                    if (compareResult.intersection !== undefined) {
+                        options.result[prop] = compareResult.intersection;
+                    }
+                    options.performedIntersection++;
+                    return true;
+                }
+            }
+        }
+    },
+    intersectionObject: function (a, b, aParent, bParent, prop, compares, options) {
+        var subsetCompare = function (a, b, aParent, bParent, prop) {
+            var compare = compares[prop] === undefined ? compares['*'] : compares[prop];
+            if (!loop(a, b, aParent, bParent, prop, compare, options)) {
+                return addIntersectedPropertyToResult(a, b, aParent, bParent, prop, compares, options);
+            }
+        };
+        var aType = typeof a;
+        if (aType === 'object' || aType === 'function') {
+            return h.eachInUnique(a, subsetCompare, b, subsetCompare, true);
+        }
+    },
+    intersectionArrayLike: function (a, b, aParent, bParent, prop, compares, options) {
+        if (h.isArrayLike(a) && h.isArrayLike(b)) {
+            var intersection = [];
+            h.each(h.makeArray(a), function (cur) {
+                for (var i = 0; i < b.length; i++) {
+                    if (compareHelpers.equal(cur, b[i], aParent, bParent, undefined, compares['*'], { 'default': false })) {
+                        intersection.push(cur);
+                        break;
+                    }
+                }
+            });
+            options.result[prop] = intersection;
+            return true;
+        }
+    }
+};
---
+++ dist/cjs/src/helpers.js
@@ -0,0 +1,213 @@
+/*src/helpers*/
+var IgnoreType = function () {
+};
+var helpers;
+module.exports = helpers = {
+    extend: function (d, s) {
+        for (var prop in s) {
+            d[prop] = s[prop];
+        }
+        return d;
+    },
+    isArrayLike: function (arr) {
+        return arr && typeof arr === 'object' && typeof arr.length === 'number' && arr.length >= 0 && (arr.length === 0 || arr.length - 1 in arr);
+    },
+    each: function (obj, cb) {
+        if (helpers.isArrayLike(obj)) {
+            for (var i = 0; i < obj.length; i++) {
+                if (cb(obj[i], i) === false) {
+                    break;
+                }
+            }
+        } else {
+            for (var prop in obj) {
+                if (obj.hasOwnProperty(prop)) {
+                    if (cb(obj[prop], prop) === false) {
+                        break;
+                    }
+                }
+            }
+        }
+        return obj;
+    },
+    eachInUnique: function (a, acb, b, bcb, defaultReturn) {
+        var bCopy = helpers.extend({}, b), res;
+        for (var prop in a) {
+            res = acb(a[prop], b[prop], a, b, prop);
+            if (res !== undefined) {
+                return res;
+            }
+            delete bCopy[prop];
+        }
+        for (prop in bCopy) {
+            res = bcb(undefined, b[prop], a, b, prop);
+            if (res !== undefined) {
+                return res;
+            }
+        }
+        return defaultReturn;
+    },
+    makeArray: function (arr) {
+        var array = [];
+        helpers.each(arr, function (item) {
+            array.push(item);
+        });
+        return array;
+    },
+    doubleLoop: function (arr, callbacks) {
+        if (typeof callbacks === 'function') {
+            callbacks = { iterate: callbacks };
+        }
+        var i = 0;
+        while (i < arr.length) {
+            if (callbacks.start) {
+                callbacks.start(arr[i]);
+            }
+            var j = i + 1;
+            while (j < arr.length) {
+                if (callbacks.iterate(arr[j], j, arr[i], i) === false) {
+                    arr.splice(j, 1);
+                } else {
+                    j++;
+                }
+            }
+            if (callbacks.end) {
+                callbacks.end(arr[i]);
+            }
+            i++;
+        }
+    },
+    identityMap: function (arr) {
+        var map = {};
+        helpers.each(arr, function (value) {
+            map[value] = 1;
+        });
+        return map;
+    },
+    arrayUnionIntersectionDifference: function (arr1, arr2) {
+        var map = {};
+        var intersection = [];
+        var union = [];
+        var difference = arr1.slice(0);
+        helpers.each(arr1, function (value) {
+            map[value] = true;
+            union.push(value);
+        });
+        helpers.each(arr2, function (value) {
+            if (map[value]) {
+                intersection.push(value);
+                var index = helpers.indexOf.call(difference, value);
+                if (index !== -1) {
+                    difference.splice(index, 1);
+                }
+            } else {
+                union.push(value);
+            }
+        });
+        return {
+            intersection: intersection,
+            union: union,
+            difference: difference
+        };
+    },
+    arraySame: function (arr1, arr2) {
+        if (arr1.length !== arr2.length) {
+            return false;
+        }
+        var map = helpers.identityMap(arr1);
+        for (var i = 0; i < arr2.length; i++) {
+            var val = map[arr2[i]];
+            if (!val) {
+                return false;
+            } else if (val > 1) {
+                return false;
+            } else {
+                map[arr2[i]]++;
+            }
+        }
+        return true;
+    },
+    indexOf: Array.prototype.indexOf || function (item) {
+        for (var i = 0, thisLen = this.length; i < thisLen; i++) {
+            if (this[i] === item) {
+                return i;
+            }
+        }
+        return -1;
+    },
+    map: Array.prototype.map || function (cb) {
+        var out = [];
+        for (var i = 0, len = this.length; i < len; i++) {
+            out.push(cb(this[i], i, this));
+        }
+        return out;
+    },
+    filter: Array.prototype.filter || function (cb) {
+        var out = [];
+        for (var i = 0, len = this.length; i < len; i++) {
+            if (cb(this[i], i, this)) {
+                out.push(this[i]);
+            }
+        }
+        return out;
+    },
+    isEmptyObject: function (obj) {
+        var prop;
+        for (prop in obj) {
+            if (obj.hasOwnProperty(prop)) {
+                break;
+            }
+        }
+        return prop === undefined;
+    },
+    ignoreType: new IgnoreType(),
+    firstProp: function (set) {
+        for (var prop in set) {
+            return prop;
+        }
+    },
+    last: function (arr) {
+        return arr[arr.length - 1];
+    },
+    index: function (compare, items, props) {
+        if (!items || !items.length) {
+            return undefined;
+        }
+        if (compare(props, items[0]) === -1) {
+            return 0;
+        } else if (compare(props, helpers.last(items)) === 1) {
+            return items.length;
+        }
+        var low = 0, high = items.length;
+        while (low < high) {
+            var mid = low + high >>> 1, item = items[mid], computed = compare(props, item);
+            if (computed === -1) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return high;
+    },
+    defaultSort: function (sortPropValue, item1, item2) {
+        var parts = sortPropValue.split(' ');
+        var sortProp = parts[0];
+        var item1Value = item1[sortProp];
+        var item2Value = item2[sortProp];
+        var temp;
+        var desc = parts[1] || '';
+        desc = desc.toLowerCase() === 'desc';
+        if (desc) {
+            temp = item1Value;
+            item1Value = item2Value;
+            item2Value = temp;
+        }
+        if (item1Value < item2Value) {
+            return -1;
+        }
+        if (item1Value > item2Value) {
+            return 1;
+        }
+        return 0;
+    }
+};
