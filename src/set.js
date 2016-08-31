8c10a39668638c0e9f7afb102b468d8978314836
Justin Meyer <justinbmeyer@gmail.com>
2016-08-31 17:37:45 -0500

    fixes #33

--- src/set.js
+++ src/set.js
@@ -1,14 +1,9 @@
 var set = require("./set-core");
+var ns = require("can-util/namespace");
 
 var props = require("./props");
 set.comparators = props; // For backward compatibility
 set.props = props;
 set.helpers = require("./helpers");
 
-
-
-if(typeof window !== "undefined" && !require.resolve) {
-	window.set = set;
-}
-
-module.exports = set;
+module.exports = ns.set = set;
