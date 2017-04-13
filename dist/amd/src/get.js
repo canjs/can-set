c30117695d7fe79b4788200118082331b8b36d9c
Nils Lundquist <nils@bitovi.com>
2017-04-13 13:05:46 -0600

    1.2.0

---
+++ dist/amd/src/get.js
@@ -0,0 +1,35 @@
+/*can-set@1.1.0#src/get*/
+define(function (require, exports, module) {
+    var compare = require('./compare');
+    var h = require('./helpers');
+    var each = require('can-util/js/each');
+    var filterData = function (data, clause, props) {
+        return h.filter.call(data, function (item) {
+            var isSubset = compare.subset(item, clause, undefined, undefined, undefined, props, {});
+            return isSubset;
+        });
+    };
+    module.exports = {
+        subsetData: function (a, b, bData, algebra) {
+            var aClauseProps = algebra.getClauseProperties(a);
+            var bClauseProps = algebra.getClauseProperties(b);
+            var options = {};
+            var aData = filterData(bData, aClauseProps.where, algebra.clauses.where);
+            if (aData.length && (aClauseProps.enabled.order || bClauseProps.enabled.order)) {
+                options = {};
+                var propName = h.firstProp(aClauseProps.order), compareOrder = algebra.clauses.order[propName];
+                aData = aData.sort(function (aItem, bItem) {
+                    return compareOrder(a[propName], aItem, bItem);
+                });
+            }
+            if (aData.length && (aClauseProps.enabled.paginate || bClauseProps.enabled.paginate)) {
+                options = {};
+                compare.subset(aClauseProps.paginate, bClauseProps.paginate, undefined, undefined, undefined, algebra.clauses.paginate, options);
+                each(options.getSubsets, function (filter) {
+                    aData = filter(a, b, aData, algebra, options);
+                });
+            }
+            return aData;
+        }
+    };
+});
