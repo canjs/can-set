7bea5332bd47c47bd0caca48d28527c01605e695
Kevin Phillips <kevin@bitovi.com>
2017-06-22 22:06:32 -0500

    1.3.0

---
+++ dist/amd/src/clause.js
@@ -0,0 +1,20 @@
+/*can-set@1.3.0-pre.0#src/clause*/
+define(function (require, exports, module) {
+    var assign = require('can-util/js/assign');
+    var each = require('can-util/js/each');
+    var clause = {};
+    module.exports = clause;
+    clause.TYPES = [
+        'where',
+        'order',
+        'paginate',
+        'id'
+    ];
+    each(clause.TYPES, function (type) {
+        var className = type.charAt(0).toUpperCase() + type.substr(1);
+        clause[className] = function (compare) {
+            assign(this, compare);
+        };
+        clause[className].type = type;
+    });
+});
