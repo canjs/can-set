0bfdcceac7f3fa63fc128e70e5d18be25034f916
imasustink <austin@austinkurpuis.com>
2017-11-01 16:32:40 -0700

    1.3.3

---
+++ dist/amd/src/set-core.js
@@ -0,0 +1,337 @@
+/*can-set@1.3.2#src/set-core*/
+define([
+    'require',
+    'exports',
+    'module',
+    './helpers',
+    './clause',
+    './compare',
+    './get',
+    'can-util/js/assign',
+    'can-util/js/each',
+    'can-util/js/make-array',
+    'can-util/js/is-empty-object'
+], function (require, exports, module) {
+    var h = require('./helpers');
+    var clause = require('./clause');
+    var compare = require('./compare');
+    var get = require('./get');
+    var assign = require('can-util/js/assign');
+    var each = require('can-util/js/each');
+    var makeArray = require('can-util/js/make-array');
+    var isEmptyObject = require('can-util/js/is-empty-object');
+    function Translate(clause, options) {
+        if (typeof options === 'string') {
+            var path = options;
+            options = {
+                fromSet: function (set, setRemainder) {
+                    return set[path] || {};
+                },
+                toSet: function (set, wheres) {
+                    set[path] = wheres;
+                    return set;
+                }
+            };
+        }
+        this.clause = clause;
+        assign(this, options);
+    }
+    var Algebra = function () {
+        var clauses = this.clauses = {
+            where: {},
+            order: {},
+            paginate: {},
+            id: {}
+        };
+        this.translators = {
+            where: new Translate('where', {
+                fromSet: function (set, setRemainder) {
+                    return setRemainder;
+                },
+                toSet: function (set, wheres) {
+                    return assign(set, wheres);
+                }
+            })
+        };
+        var self = this;
+        each(arguments, function (arg) {
+            if (arg) {
+                if (arg instanceof Translate) {
+                    self.translators[arg.clause] = arg;
+                } else {
+                    assign(clauses[arg.constructor.type || 'where'], arg);
+                }
+            }
+        });
+    };
+    Algebra.make = function (compare, count) {
+        if (compare instanceof Algebra) {
+            return compare;
+        } else {
+            return new Algebra(compare, count);
+        }
+    };
+    assign(Algebra.prototype, {
+        getClauseProperties: function (set, options) {
+            options = options || {};
+            var setClone = assign({}, set);
+            var clauses = this.clauses;
+            var checkClauses = [
+                'order',
+                'paginate',
+                'id'
+            ];
+            var clauseProps = {
+                enabled: {
+                    where: true,
+                    order: false,
+                    paginate: false,
+                    id: false
+                }
+            };
+            if (options.omitClauses) {
+                checkClauses = h.arrayUnionIntersectionDifference(checkClauses, options.omitClauses).difference;
+            }
+            each(checkClauses, function (clauseName) {
+                var valuesForClause = {};
+                var prop;
+                for (prop in clauses[clauseName]) {
+                    if (prop in setClone) {
+                        valuesForClause[prop] = setClone[prop];
+                        if (clauseName !== 'id') {
+                            delete setClone[prop];
+                        }
+                    }
+                }
+                clauseProps[clauseName] = valuesForClause;
+                clauseProps.enabled[clauseName] = !isEmptyObject(valuesForClause);
+            });
+            clauseProps.where = options.isProperties ? setClone : this.translators.where.fromSet(set, setClone);
+            return clauseProps;
+        },
+        getDifferentClauseTypes: function (aClauses, bClauses) {
+            var self = this;
+            var differentTypes = [];
+            each(clause.TYPES, function (type) {
+                if (!self.evaluateOperator(compare.equal, aClauses[type], bClauses[type], { isProperties: true }, { isProperties: true })) {
+                    differentTypes.push(type);
+                }
+            });
+            return differentTypes;
+        },
+        updateSet: function (set, clause, result, useSet) {
+            if (result && typeof result === 'object' && useSet !== false) {
+                if (this.translators[clause]) {
+                    set = this.translators.where.toSet(set, result);
+                } else {
+                    set = assign(set, result);
+                }
+                return true;
+            } else if (result) {
+                return useSet === undefined ? undefined : false;
+            } else {
+                return false;
+            }
+        },
+        evaluateOperator: function (operator, a, b, aOptions, bOptions, evaluateOptions) {
+            aOptions = aOptions || {};
+            bOptions = bOptions || {};
+            evaluateOptions = assign({
+                evaluateWhere: operator,
+                evaluatePaginate: operator,
+                evaluateOrder: operator,
+                shouldEvaluatePaginate: function (aClauseProps, bClauseProps) {
+                    return aClauseProps.enabled.paginate || bClauseProps.enabled.paginate;
+                },
+                shouldEvaluateOrder: function (aClauseProps, bClauseProps) {
+                    return aClauseProps.enabled.order && compare.equal(aClauseProps.order, bClauseProps.order, undefined, undefined, undefined, {}, {});
+                }
+            }, evaluateOptions || {});
+            var aClauseProps = this.getClauseProperties(a, aOptions), bClauseProps = this.getClauseProperties(b, bOptions), set = {}, useSet;
+            var result = evaluateOptions.evaluateWhere(aClauseProps.where, bClauseProps.where, undefined, undefined, undefined, this.clauses.where, {});
+            useSet = this.updateSet(set, 'where', result, useSet);
+            if (result && evaluateOptions.shouldEvaluatePaginate(aClauseProps, bClauseProps)) {
+                if (evaluateOptions.shouldEvaluateOrder(aClauseProps, bClauseProps)) {
+                    result = evaluateOptions.evaluateOrder(aClauseProps.order, bClauseProps.order, undefined, undefined, undefined, {}, {});
+                    useSet = this.updateSet(set, 'order', result, useSet);
+                }
+                if (result) {
+                    result = evaluateOptions.evaluatePaginate(aClauseProps.paginate, bClauseProps.paginate, undefined, undefined, undefined, this.clauses.paginate, {});
+                    useSet = this.updateSet(set, 'paginate', result, useSet);
+                }
+            } else if (result && evaluateOptions.shouldEvaluateOrder(aClauseProps, bClauseProps)) {
+                result = operator(aClauseProps.order, bClauseProps.order, undefined, undefined, undefined, {}, {});
+                useSet = this.updateSet(set, 'order', result, useSet);
+            }
+            return result && useSet ? set : result;
+        },
+        equal: function (a, b) {
+            return this.evaluateOperator(compare.equal, a, b);
+        },
+        subset: function (a, b) {
+            var aClauseProps = this.getClauseProperties(a);
+            var bClauseProps = this.getClauseProperties(b);
+            var compatibleSort = true;
+            var result;
+            if (bClauseProps.enabled.paginate && (aClauseProps.enabled.order || bClauseProps.enabled.order)) {
+                compatibleSort = compare.equal(aClauseProps.order, bClauseProps.order, undefined, undefined, undefined, {}, {});
+            }
+            if (!compatibleSort) {
+                result = false;
+            } else {
+                result = this.evaluateOperator(compare.subset, a, b);
+            }
+            return result;
+        },
+        properSubset: function (a, b) {
+            return this.subset(a, b) && !this.equal(a, b);
+        },
+        difference: function (a, b) {
+            var aClauseProps = this.getClauseProperties(a);
+            var bClauseProps = this.getClauseProperties(b);
+            var differentClauses = this.getDifferentClauseTypes(aClauseProps, bClauseProps);
+            var result;
+            switch (differentClauses.length) {
+            case 0: {
+                    result = false;
+                    break;
+                }
+            case 1: {
+                    var clause = differentClauses[0];
+                    result = compare.difference(aClauseProps[clause], bClauseProps[clause], undefined, undefined, undefined, this.clauses[clause], {});
+                    if (this.translators[clause] && typeof result === 'object') {
+                        result = this.translators[clause].toSet({}, result);
+                    }
+                    break;
+                }
+            }
+            return result;
+        },
+        union: function (a, b) {
+            return this.evaluateOperator(compare.union, a, b);
+        },
+        intersection: function (a, b) {
+            return this.evaluateOperator(compare.intersection, a, b);
+        },
+        count: function (set) {
+            return this.evaluateOperator(compare.count, set, {});
+        },
+        has: function (set, props) {
+            var aClauseProps = this.getClauseProperties(set);
+            var propsClauseProps = this.getClauseProperties(props, { isProperties: true });
+            var compatibleSort = true;
+            var result;
+            if ((propsClauseProps.enabled.paginate || aClauseProps.enabled.paginate) && (propsClauseProps.enabled.order || aClauseProps.enabled.order)) {
+                compatibleSort = compare.equal(propsClauseProps.order, aClauseProps.order, undefined, undefined, undefined, {}, {});
+            }
+            if (!compatibleSort) {
+                result = false;
+            } else {
+                result = this.evaluateOperator(compare.subset, props, set, { isProperties: true }, undefined, {
+                    shouldEvaluatePaginate: function () {
+                        return false;
+                    }
+                });
+            }
+            return result;
+        },
+        index: function (set, items, item) {
+            var aClauseProps = this.getClauseProperties(set);
+            var propName = h.firstProp(aClauseProps.order), compare, orderValue;
+            if (propName) {
+                compare = this.clauses.order[propName];
+                orderValue = set[propName];
+                return h.index(function (itemA, itemB) {
+                    return compare(orderValue, itemA, itemB);
+                }, items, item);
+            }
+            propName = h.firstProp(this.clauses.id);
+            if (propName) {
+                compare = h.defaultSort;
+                orderValue = propName;
+                return h.index(function (itemA, itemB) {
+                    return compare(orderValue, itemA, itemB);
+                }, items, item);
+            }
+            return;
+        },
+        getSubset: function (a, b, bData) {
+            var aClauseProps = this.getClauseProperties(a);
+            var bClauseProps = this.getClauseProperties(b);
+            var isSubset = this.subset(assign({}, aClauseProps.where, aClauseProps.paginate), assign({}, bClauseProps.where, bClauseProps.paginate));
+            if (isSubset) {
+                return get.subsetData(a, b, bData, this);
+            }
+        },
+        getUnion: function (a, b, aItems, bItems) {
+            var aClauseProps = this.getClauseProperties(a);
+            var bClauseProps = this.getClauseProperties(b);
+            var algebra = this;
+            var options;
+            if (this.subset(a, b)) {
+                return bItems;
+            } else if (this.subset(b, a)) {
+                return aItems;
+            }
+            var combined;
+            if (aClauseProps.enabled.paginate || bClauseProps.enabled.paginate) {
+                options = {};
+                var isUnion = compare.union(aClauseProps.paginate, bClauseProps.paginate, undefined, undefined, undefined, this.clauses.paginate, options);
+                if (!isUnion) {
+                    return;
+                } else {
+                    each(options.getUnions, function (filter) {
+                        var items = filter(a, b, aItems, bItems, algebra, options);
+                        aItems = items[0];
+                        bItems = items[1];
+                    });
+                    combined = aItems.concat(bItems);
+                }
+            } else {
+                combined = aItems.concat(bItems);
+            }
+            if (combined.length && aClauseProps.enabled.order && compare.equal(aClauseProps.order, bClauseProps.order, undefined, undefined, undefined, {}, {})) {
+                options = {};
+                var propName = h.firstProp(aClauseProps.order), compareOrder = algebra.clauses.order[propName];
+                combined = combined.sort(function (aItem, bItem) {
+                    return compareOrder(a[propName], aItem, bItem);
+                });
+            }
+            return combined;
+        },
+        id: function (props) {
+            var keys = Object.keys(this.clauses.id);
+            if (keys.length === 1) {
+                return props[keys[0]];
+            } else {
+                var id = {};
+                keys.forEach(function (key) {
+                    id[key] = props[key];
+                });
+                return JSON.stringify(id);
+            }
+        }
+    });
+    var callOnAlgebra = function (methodName, algebraArgNumber) {
+        return function () {
+            var args = makeArray(arguments).slice(0, algebraArgNumber);
+            var algebra = Algebra.make(arguments[algebraArgNumber]);
+            return algebra[methodName].apply(algebra, args);
+        };
+    };
+    module.exports = {
+        Algebra: Algebra,
+        Translate: Translate,
+        difference: callOnAlgebra('difference', 2),
+        equal: callOnAlgebra('equal', 2),
+        subset: callOnAlgebra('subset', 2),
+        properSubset: callOnAlgebra('properSubset', 2),
+        union: callOnAlgebra('union', 2),
+        intersection: callOnAlgebra('intersection', 2),
+        count: callOnAlgebra('count', 1),
+        has: callOnAlgebra('has', 2),
+        index: callOnAlgebra('index', 3),
+        getSubset: callOnAlgebra('getSubset', 3),
+        getUnion: callOnAlgebra('getUnion', 4)
+    };
+});
