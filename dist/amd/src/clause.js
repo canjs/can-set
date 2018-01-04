4b4c541c9f476b35b927d3f5934447c39174b27f
Bradley Momberger <brad@bitovi.com>
2018-01-04 17:51:21 -0500

    1.5.0

---
+++ dist/amd/src/clause.js
@@ -0,0 +1,26 @@
+/*can-set@1.4.0#src/clause*/
+define([
+    'require',
+    'exports',
+    'module',
+    'can-util/js/assign',
+    'can-util/js/each'
+], function (require, exports, module) {
+    var assign = require('can-util/js/assign');
+    var each = require('can-util/js/each');
+    var clause = {};
+    module.exports = clause;
+    clause.TYPES = [
+        'where',
+        'order',
+        'paginate',
+        'id'
+    ];
+    each(clause.TYPES, function (type) {
+        var className = type.charAt(0).toUpperCase() + type.substr(1);
+        clause[className] = function (compare) {
+            assign(this, compare);
+        };
+        clause[className].type = type;
+    });
+});
