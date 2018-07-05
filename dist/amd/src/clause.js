37c0799dd2d2365dbb4a83de25f1a1a08cfe75a7
Austin Sanders <austinsanders@Austins-MacBook-Pro.local>
2018-07-05 15:25:36 -0700

    1.6.0

---
+++ dist/amd/src/clause.js
@@ -0,0 +1,27 @@
+/*can-set@1.5.2#src/clause*/
+define([
+    'require',
+    'exports',
+    'module',
+    'can-util/js/assign',
+    'can-util/js/each'
+], function (require, exports, module) {
+    'use strict';
+    var assign = require('can-util/js/assign');
+    var each = require('can-util/js/each');
+    var clause = {};
+    module.exports = clause;
+    clause.TYPES = [
+        'where',
+        'order',
+        'paginate',
+        'id'
+    ];
+    each(clause.TYPES, function (type) {
+        var className = type.charAt(0).toUpperCase() + type.substr(1);
+        clause[className] = function (compare) {
+            assign(this, compare);
+        };
+        clause[className].type = type;
+    });
+});
